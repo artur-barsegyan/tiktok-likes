8f54b8f8a5b1821005c1e0bd4dfa10b958364eb5
Artur Barsegyan <a.barsegyan96@gmail.com>
2020-11-18 15:51:24 +0300

    docker: create first buildable state

--- app/roles/custom.lua
+++ app/roles/custom.lua
@@ -3,51 +3,50 @@ local crud = require('crud')
 local uuid = require('uuid')
 local json = require('json')
 
-local function create_response(obj, err)
-    local resp
+function add_user(request)
+    local fullname = request:post_param("fullname")
+    local result, err = crud.insert_object('users', { user_id = uuid.new(), fullname = fullname })
     if err ~= nil then
-        resp = { body = json.encode({status = "Error!", error = err}), status = 500 }
-    else
-        resp = { body = json.encode({status = "Success!", result = obj}), status = 200 }
+        return { body = json.encode({status = "Error!", error = err}), status = 500 }
     end
 
-    return resp
+    return { body = json.encode({status = "Success!", result = result}), status = 200 }
 end
 
-function create_user(req)
-    local fullname = req:post_param("fullname")
-    local obj, err = crud.insert_object('users', { user_id = uuid.new(), fullname = fullname })
-
-    return create_response(obj, err)
-end
-
-function add_video(req)
-    local description = req:post_param("description")
-    local obj, err = crud.insert_object('videos', { video_id = uuid.new(), description = description, likes = 0 })
-
-    return create_response(obj, err)
-end
-
-function like_video(req)
-    local video_id = req:post_param("video_id")
-    local user_id = req:post_param("user_id")
-
-    -- TODO: 2 or more writers simultaneously
-    local obj, err = crud.update('videos', uuid.fromstr(video_id), {{'+', 'likes', 1}})
-    if err == nil then
-        obj, err = crud.insert_object('likes', { like_id = uuid.new(),
-                                                      video_id = uuid.fromstr(video_id),
-                                                      user_id = uuid.fromstr(user_id)})
+function add_video(request)
+    local description = request:post_param("description")
+    local result, err = crud.insert_object('videos', { video_id = uuid.new(), description = description, likes = 0 })
+    if err ~= nil then
+        return { body = json.encode({status = "Error!", error = err}), status = 500 }
     end
 
-    return create_response(obj, err)
+    return { body = json.encode({status = "Success!", result = result}), status = 200 }
+end
+
+function like_video(request)
+    local video_id = request:post_param("video_id")
+    local user_id = request:post_param("user_id")
+
+    local result, err = crud.update('videos', uuid.fromstr(video_id), {{'+', 'likes', 1}})
+    if err ~= nil then
+        return { body = json.encode({status = "Error!", error = err}), status = 500 }
+    end
+
+    result, err = crud.insert_object('likes', { like_id = uuid.new(),
+                                                video_id = uuid.fromstr(video_id),
+                                                user_id = uuid.fromstr(user_id)})
+    if err ~= nil then
+        return { body = json.encode({status = "Error!", error = err}), status = 500 }
+    end
+
+    return { body = json.encode({status = "Success!", result = result}), status = 200 }
 end
 
 
 local function init(opts) -- luacheck: no unused args
     local httpd = cartridge.service_get('httpd')
     httpd:route({path = '/like', method = 'POST'}, like_video)
-    httpd:route({path = '/create_user', method = 'POST'}, create_user)
+    httpd:route({path = '/add_user', method = 'POST'}, add_user)
     httpd:route({path = '/add_video', method = 'POST'}, add_video)
 
     return true
--- tiktok-likes-scm-1.rockspec
+++ tiktok-likes-scm-1.rockspec
@@ -10,8 +10,8 @@ dependencies = {
 	'checks == 3.0.1-1',
     'cartridge == 2.3.0-1',
     'metrics == 0.5.0-1',
-    'cartridge-cli-extensions == 1.0.0-1',
-    "crud == 0.3.0"
+    'crud == 0.3.0',
+    'space-explorer == scm-1'
 }
 build = {
 	type = 'none';
